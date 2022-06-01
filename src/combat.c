4d779850c8a6411cd803128b623ceb3bcf046e94
Fiona <cleo9501@gmail.com>
2022-06-01 09:14:43 +0200

    combat

--- src/combat.c
+++ src/combat.c
@@ -8,5 +8,22 @@ void fight1(){  //procédure de combat d'un Joueur VS Machine
 void fight2(Team t1, Team t2){  //procédure de combat d'un Joueurs VS un Joueur
   while((t1.players > 0) && (t2.players > 0)){
     
+    
   }
+}
+
+void mode_1(Team t1){ //mode de jeux facile
+
+}
+
+void mode_2(Team t1){  //mode de jeux moyen
+
+}
+
+void mode_3(Team t1){ //mode de jeux difficile
+
+}
+
+void mode_4(Team t1){ //mode  de jeux torture
+
 }
