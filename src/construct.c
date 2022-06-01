d49c5448728e819ef67257b6b4bc2cc501b62731
zeyneb <zeineb.chaabane98@gmail.com>
2022-06-01 10:03:23 +0200

    new version construct.c

--- src/construct.c
+++ src/construct.c
@@ -1,5 +1,5 @@
 #include "struct.h"
-Special construct_spe(int pos_ligne){  //constructeur de la structure Special
+Special construct_spe(char *nom){  //constructeur de la structure Special
   Special s;
   int l,c,n=0;
   char buffer[1000];
@@ -9,14 +9,11 @@ Special construct_spe(int pos_ligne){  //constructeur de la structure Special
     printf("Ouverture du fichier impossible\n");
     exit(1);
   }
-  c=fgetc(fichier1);
-  while (n!=pos_ligne){
-    while (c!='\n'){
-      fgetc(fichier1);
-    }
-  n++;
-  }
   fscanf(fichier1, "%s", buffer);//récupération du premier mot 
+  while (strcmp(buffer,nom)!=0){
+    fseek(fichier1,9,1);
+    fscanf(fichier1,"%s",buffer);
+  }
   l=strlen(buffer);
   s.nom=malloc((l+1)*sizeof(char));
   if (s.nom==NULL){ //vérification allocation mémoire
@@ -24,15 +21,20 @@ Special construct_spe(int pos_ligne){  //constructeur de la structure Special
     exit(1);
   }
   strcpy(s.nom,buffer);// copie de la chaine de buffer dans nom
+  s.nom[l]='\0';
   fscanf(fichier1,"%d",&s.spe_type);// récupération de la valeur du type du fichier texte 
-  fscanf(fichier1,"%d", &s.valeur); // récupération de la valeur du coup spécial du fichier texte
+  fscanf(fichier1,"%d",&s.valeur); // récupération de la valeur du coup spécial du fichier texte
+  fscanf(fichier1,"%d",&s.nmb_tourActif);//récupération du nombre de tours actifs de l'effet spécial
+  fscanf(fichier1,"%d",&s.nmb_tourRecharge);//récupération du nombre de tours de recharge 
   fclose (fichier1);// fermeture du fichier
   return s;
 }
 
+
 Fighter construct_fighter(){ //constructeur de la structure Fighter
   Fighter f;
   FILE *fichier2;
+  char *nom_effet;
   fichier2=fopen("../file/combattants.txt","r+");// ouverture du fichier texte des effets spéciaux
   if(fichier2==NULL){//verification ouverture du fichier
     printf("Ouverture du fichier impossible\n");
@@ -46,7 +48,8 @@ Fighter construct_fighter(){ //constructeur de la structure Fighter
   fscanf(fichier2,"%d",&f.defense);// récupérer la valeur des défenses du fichier texte
   f.dodge=rand()%11 +1; 
   fscanf(fichier2,"%d",&f.speed);// récupérer la valeur de la vitesse du fichier texte
-  fscanf(fichier2, "%d", f.sp_attack);// récupérer l'effet special du combattant
+  fscanf(fichier2, "%s", nom_effet);// récupérer le nom de l'effet spécial
+  f.sp_attack=construct_spe(nom_effet); // constructeur de l'effet spécial 
   fscanf(fichier2,"%d",&f.spe_effects);//récupérer la valeur de la durée de l'effet 
   fclose(fichier2);// fermeture du fichier 
   return f;
