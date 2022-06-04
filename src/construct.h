edf6e3398448d2091c349ab469e399640e638607
Fiona <cleo9501@gmail.com>
2022-06-04 16:04:30 +0200

    mess

--- src/construct.h
+++ src/construct.h
@@ -1,4 +1,7 @@
 #include "struct.h"
+
 Special construct_spe(char *nom);
+
 Fighter construct_fighter(char *nom_fighter);
+
 Team construct_team(int n);
---
+++ src/display.h
@@ -0,0 +1,5 @@
+#include"struct.h"
+
+void disp_align(char chain[]);
+
+void display(Team t1, Team t2, Fighter first_a);
