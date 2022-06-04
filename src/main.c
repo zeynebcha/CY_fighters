4bc1f692ab475dcefdd077f3ef4ffa5060d8ea44
Fiona <cleo9501@gmail.com>
2022-06-04 15:36:42 +0200

    mess

--- src/main.c
+++ src/main.c
@@ -12,26 +12,42 @@ int main(void){
     printf("Quel mode de jeux voulez-vous ?\n1 : pour Joueur VS Machine \n2 : pour Joueur VS Joueur");
   scanf("%d", &mode_jeu);
   }while((mode_jeu != 1) || (mode_jeu != 2));  //test de vérification
-  if (mode_jeu==1){
-  do{
-    printf("Choix du mode de difficulté : \n1 : pour Noob \n2 : pour Facile \n3 : pour Moyen \n4 : pour Difficile \n");
-    scanf("%d", &dif);
-  }while((dif < 1) || (dif > 4));// test de vérification
-  if(dif >= 1 && dif <= 3){
-      // équipe machine généré aléatoirement 
+  if (mode_jeu==1){ //mode joueur vs machine
+    Team computer;
+    do{
+      printf("Choix du mode de difficulté : \n1 : pour Noob \n2 : pour Facile \n3 : pour Moyen \n4 : pour Difficile \n");
+      scanf("%d", &dif);
+    }while((dif < 1) || (dif > 4));// test de vérification
+    if(dif >= 1 && dif <= 3){
+        // équipe machine généré aléatoirement 
+        combat(t1, computer, 1, dif);
+        if(dif == 1){
+
+        }
+        else if(dif ==2){
+
+        }
+        else if(dif ==3){
+
+        }
+    }
+    else{
+        // équipe prédéfinie
+        team_md = rand()%3 + 1;
+        if(team_md == 1){
+          printf("Le joueur va jouer contre l'équipe prédéfinie 1 contenant Daera, Miroveed, Iela et Arel\n");
+
+        }
+        if(team_md == 2){
+          printf("Le joueur va jouer contre l'équipe prédéfinie 2 contenant Nallmar, Ada, Deam et Lorel\n");
+        }
+        if(team_md == 3){
+          printf("Le joueur va jouer contre l'équipe prédéfinie 3 contenant Tagriel, Nallmar, Arrion et Daera\n");
+        }
+        combat(t1, computer, 1, dif);
+    }
   }
-  else{
-      // équipe prédéfinie
-      team_md = rand()%3 + 1;
-      if(team_md == 1){
-        printf("Le joueur va jouer contre l'équipe prédéfinie 1 contenant Daera, Miroveed, Iela et Arel\n");
-      }
-      if(team_md == 2){
-        printf("Le joueur va jouer contre l'équipe prédéfinie 2 contenant Nallmar, Ada, Deam et Lorel\n");
-      }
-      if(team_md == 3){
-        printf("Le joueur va jouer contre l'équipe prédéfinie 3 contenant Tagriel, Nallmar, Arrion et Daera\n");
-      }
+  else{ //mode joueur vs joueur 
+
   }
-}
 }
