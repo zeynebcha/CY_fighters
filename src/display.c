bed5bc33b0da151f72c1ba311205accac7800429
Fiona <cleo9501@gmail.com>
2022-06-04 16:21:09 +0200

    mess

--- src/display.c
+++ src/display.c
@@ -1,25 +1,26 @@
 #include"struct.h"
 #include"combat.h"
 
-
+//procédure pour aligner les encadrés dans display
 void disp_align(char chain[]){
-  int compt = strlen(chain);
-    for(int i = 0; i < compt;i++){
-      printf("_");
+  int compt = strlen(chain);  //récupère la longueur de la chaîne de caractères
+    for(int i = 0; i < compt;i++){  
+      printf("_");  //compense les caractères de la chaîne avec des _ pour aligner les bords de l'encadré
     }
     printf("\n");
   }    
 
-void display(Team t1, Team t2, Fighter first_a){  //procédure pour afficher l'interface graphique
-printf("_[%s]______________________________\n\n", t1.name);   //1ère équipe - adverse
+//procédure pour afficher l'interface graphique
+void display(Team t1, Team t2, Fighter first_a){  
+printf("_[%s]______________________________\n\n", t1.name);   //1ère équipe - adverse (début de l'encadré)
   for(int i=0; i < t1.players;i++){
     if(t1.f[i].health == 0){
       printf("   %s |%s|\n", t1.f[i].name, "mort"); //affiche le nom et mort s'il le combattant n'a plus de pv
     }
     else{
-      printf("   %s |%d|\n", t1.f[i].name, i);  // affiche le nom et le numéro s'il le combattant est vivant
+      printf("   %s |%d|\n", t1.f[i].name, i);  //affiche le nom et le numéro s'il le combattant est vivant
     }
-    
+    //affiche les effets spéciaux que subit le combattant
     if(t1.f[i].spe_effects == 0){
       printf("   Subit des dégats\n");   
     }
@@ -29,20 +30,21 @@ printf("_[%s]______________________________\n\n", t1.name);   //1ère équipe - 
     if(t1.f[i].spe_effects == 2){
       printf("   Reçoit du boost\n");
     }
-    printf("   Vie : %d\n",t1.f[i].health);
+    printf("   Vie : %d\n",t1.f[i].health); //affiche la vie du combattant
     }
   printf("_________________________________");
-  disp_align(t1.name);  //pour aligner la dernière ligne d'affichage à la 1ère
+  disp_align(t1.name);  //pour aligner la dernière ligne d'affichage à la 1ère (fin de l'encadré)
 
 
-printf("_[%s]______________________________\n\n", t2.name);   //2nd équipe - joueur
+printf("_[%s]______________________________\n\n", t2.name);   //2nd équipe - joueur (début de l'encadré)
   for(int i=0; i < t2.players;i++){
     if(t2.f[i].name == first_a.name){
-      printf("   )>%s<( \n", t2.f[i].name);  // affiche le nom de l'attaquant actuel
+      printf("   )>%s<( \n", t2.f[i].name);  // affiche le nom de l'attaquant actuel avec des signes pour le rendre visible
     }
     else{
       printf("   %s \n", t2.f[i].name);  // affiche le nom 
     }
+    //affiches les effets que subit le combattant
     if(t2.f[i].spe_effects == 0){
       printf("   Subit des dégats\n");   
     }
@@ -52,9 +54,9 @@ printf("_[%s]______________________________\n\n", t2.name);   //2nd équipe - jo
     if(t2.f[i].spe_effects == 2){
       printf("   Reçoit du boost\n");
     }
-    printf("   Vie : %d\n",t2.f[i].health);
+    printf("   Vie : %d\n",t2.f[i].health); //affiche la vie du combattant
     }
-  printf("   %s \nTechniques spéciales\n", first_a.name); //affiche le nom du combattant actuel et techniques spéciales
+  printf("   %s \nTechniques spéciales\n", first_a.name); //affiche le nom du combattant actuel et ses techniques spéciales
   if(first_a.sp_attack.nmb_tourRecharge != 0){
     printf("   |1| %s durée : %d tour", first_a.sp_attack.nom, first_a.sp_attack.nmb_tourActif);
   }
@@ -63,5 +65,5 @@ printf("_[%s]______________________________\n\n", t2.name);   //2nd équipe - jo
   }
   
   printf("_________________________________");
-  disp_align(t2.name);  //pour aligner la dernière ligne d'affichage à la 1ère
+  disp_align(t2.name);  //pour aligner la dernière ligne d'affichage à la 1ère (fin de l'encadré)
 }
